298410062b628ab0cb7d58c8c4d67b341cbfc30f
Paulo Vera <51829447+PauloACH@users.noreply.github.com>
2023-04-10 18:58:17 -0400

    first commit

---
+++ src/app/shared/data/proyectos.data.ts
@@ -0,0 +1,34 @@
+export const proyectos: any[] = [
+    {
+      nombre: 'Contador',
+      img: '/assets/contador.png',
+      tecnologias: ['Angular Framework', 'CSS', 'PWA'],
+      descripcion:
+        'Aplicacion de contador aprendiendo Angular y a manejar eventos y reactividad',
+      url: 'https://fancy-daffodil-239ede.netlify.app',
+    },
+    {
+      nombre: 'Pokeapp',
+      img: '/assets/pokeapp.png',
+      tecnologias: ['Angular Framework', 'Angular Material', 'PWA'],
+      descripcion:
+        'Aplicacion de pokemon usando Angular Material para los estilos',
+      url: 'https://monumental-lokum-3d43f6.netlify.app',
+    },
+    {
+      nombre: 'Listado de Tareas',
+      img: '/assets/tareas.png',
+      tecnologias: ['Angular Framework', 'Bulma CSS'],
+      descripcion:
+        'Aplicacion de listado de tareas usando Bulma CSS como Framework para los estilos',
+      url: 'https://tiny-hummingbird-6a7cfa.netlify.app',
+    },
+    {
+      nombre: 'Pipes App',
+      img: '/assets/pipes.png',
+      tecnologias: ['Angular Framework', 'Bootstrap 5'],
+      descripcion:
+        'Aplicacion de listado de tareas usando Bootstrap 5 como Framework para los estilos',
+      url: 'https://cosmic-biscuit-c063ea.netlify.app',
+    },
+  ];
